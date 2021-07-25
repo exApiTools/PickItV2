48416a7f26872887cfb27bfc121f789ce3bc261c
vadash <vadash@gmail.com>
2021-07-25 12:39:27 +0300

    =Fix for new patch

--- CustomItem.cs
+++ CustomItem.cs
@@ -25,121 +25,12 @@ namespace PickIt
             if (worldItem == null) return;
             var groundItem = worldItem.ItemEntity;
             GroundItem = groundItem;
-            Path = groundItem?.Path;
             if (GroundItem == null) return;
-
-            if (Path != null && Path.Length < 1)
-            {
-                DebugWindow.LogMsg($"World: {worldItem.Address:X} P: {Path}", 2);
-                DebugWindow.LogMsg($"Ground: {GroundItem.Address:X} P {Path}", 2);
-                return;
-            }
-
             IsTargeted = () => itemItemOnGround?.GetComponent<Targetable>()?.isTargeted == true;
-
-            var baseItemType = fs.BaseItemTypes.Translate(Path);
-
-            if (baseItemType != null)
-            {
-                ClassName = baseItemType.ClassName;
-                BaseName = baseItemType.BaseName;
-                Width = baseItemType.Width;
-                Height = baseItemType.Height;
-                if (weightsRules.TryGetValue(BaseName, out var w)) Weight = w;
-                if (ClassName.StartsWith("Heist")) IsHeist = true;
-            }
-
-            var WeaponClass = new List<string>
-            {
-                "One Hand Mace",
-                "Two Hand Mace",
-                "One Hand Axe",
-                "Two Hand Axe",
-                "One Hand Sword",
-                "Two Hand Sword",
-                "Thrusting One Hand Sword",
-                "Bow",
-                "Claw",
-                "Dagger",
-                "Rune Dagger",
-                "Sceptre",
-                "Staff",
-                "Wand"
-            };
-
-            if (GroundItem.HasComponent<Quality>())
-            {
-                var quality = GroundItem.GetComponent<Quality>();
-                Quality = quality.ItemQuality;
-            }
-
-            if (GroundItem.HasComponent<Base>())
-            {
-                var @base = GroundItem.GetComponent<Base>();
-                IsElder = @base.isElder;
-                IsShaper = @base.isShaper;
-                IsHunter = @base.isHunter;
-                IsRedeemer = @base.isRedeemer;
-                IsCrusader = @base.isCrusader;
-                IsWarlord = @base.isWarlord;
-            }
-
-            if (GroundItem.HasComponent<Mods>())
-            {
-                var mods = GroundItem.GetComponent<Mods>();
-                Rarity = mods.ItemRarity;
-                IsIdentified = mods.Identified;
-                ItemLevel = mods.ItemLevel;
-                IsVeiled = mods.ItemMods.Any(m => m.DisplayName.Contains("Veil"));
-            }
-
-            if (GroundItem.HasComponent<Sockets>())
-            {
-                var sockets = GroundItem.GetComponent<Sockets>();
-                IsRGB = sockets.IsRGB;
-                Sockets = sockets.NumberOfSockets;
-                LargestLink = sockets.LargestLinkSize;
-            }
-
-            if (GroundItem.HasComponent<Weapon>()) IsWeapon = true;
-
-            MapTier = GroundItem.HasComponent<Map>() ? GroundItem.GetComponent<Map>().Tier : 0;
             IsValid = true;
         }
-
-        public string BaseName { get; } = "";
-        public string ClassName { get; } = "";
         public LabelOnGround LabelOnGround { get; }
         public float Distance { get; }
         public Entity GroundItem { get; }
-
-        public MinimapIcon WorldIcon { get; }
-        public int Height { get; }
-        public bool IsElder { get; }
-        public bool IsIdentified { get; }
-        public bool IsRGB { get; }
-        public bool IsShaper { get; }
-        public bool IsHunter { get; }
-        public bool IsRedeemer { get; }
-        public bool IsCrusader { get; }
-        public bool IsWarlord { get; }
-        public bool IsHeist { get; }
-        public bool IsVeiled { get; }
-        public bool IsWeapon { get; }
-        public int ItemLevel { get; }
-        public int LargestLink { get; }
-        public int MapTier { get; }
-        public string Path { get; }
-        public int Quality { get; }
-        public ItemRarity Rarity { get; }
-        public int Sockets { get; }
-        public int Width { get; }
-        public int Weight { get; set; }
-        public bool IsMetaItem { get; set; }
-
-        public override string ToString()
-        {
-            return $"{BaseName} ({ClassName}) W: {Weight} Dist: {Distance}";
-        }
     }
 }
--- PickIt.cs
+++ PickIt.cs
@@ -127,10 +127,13 @@ namespace PickIt
             var window = GameController.Window.GetWindowRectangleTimeCache;
             var rect = new RectangleF(window.X, window.X, window.X + window.Width, window.Y + window.Height);
             var labels = GameController.Game.IngameState.IngameUi.ItemsOnGroundLabelsVisible
-                .Where(x => x.Address != 0 &&
-                    x.ItemOnGround?.Path != null &&
-                    x.IsVisible && x.Label.GetClientRectCache.Center.PointInRectangle(rect) &&
-                    x.CanPickUp && x.MaxTimeForPickUp.TotalSeconds <= 0)
+                .Where(x => x.Address != 0
+                    //&& x.ItemOnGround?.Path != null 
+                    //&& x.IsVisible 
+                    //&& x.Label.GetClientRectCache.Center.PointInRectangle(rect) 
+                    //&& x.CanPickUp 
+                    //&& x.MaxTimeForPickUp.TotalSeconds <= 0
+                    )
                 .Select(x => new CustomItem(x, GameController.Files,
                     x.ItemOnGround.DistancePlayer, _weightsRules))
                 .OrderBy(x => x.Distance).ToList();
